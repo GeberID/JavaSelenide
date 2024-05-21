ea03a411577f0b75d574e3e7daecb705cb4f9d71
Александр <guslyannikov_a_e@ntik.ru>
2024-05-21 15:11:28 +0300

    add buyBikeLight tests

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -36,5 +38,28 @@ class AutoTest extends BaseTest{
         $("span[data-test=\"shopping-cart-badge\"]").should(Condition.text("1"));
         $("a[data-test=\"shopping-cart-link\"]").click();
         $$("div[data-test=\"inventory-item-name\"]").findBy(Condition.text("Sauce Labs Bolt T-Shirt")).should(Condition.visible);
+    }
+    @Test
+    @Tag("AUTOTEST")
+    void buyBikeLight(){
+        $$("div[data-test=\"inventory-item-name\"]").findBy(Condition.text("Sauce Labs Bike Light")).click();
+        $("button[data-test=\"add-to-cart\"]").click();
+        $("a[data-test=\"shopping-cart-link\"] span[data-test=\"shopping-cart-badge\"]").should(exist,Condition.text("1"));
+        $("a[data-test=\"shopping-cart-link\"]").click();
+        $("div[data-test=\"inventory-item-name\"]").has(Condition.text("Sauce Labs Bike Light"));
+        $("button[data-test=\"remove-sauce-labs-bike-light\"]").is(exist);
+        $("button[data-test=\"checkout\"]").click();
+
+        $("input[data-test=\"firstName\"]").sendKeys("Tester");
+        $("input[data-test=\"lastName\"]").sendKeys("Tester");
+        $("input[data-test=\"postalCode\"]").sendKeys("12345");
+        $("input[data-test=\"continue\"]").click();
+
+        $("div[data-test=\"payment-info-value\"]").has(text("SauceCard #31337"));
+        $("div[data-test=\"shipping-info-value\"]").has(text("Free Pony Express Delivery!"));
+        $("button[data-test=\"finish\"]").click();
+
+        $("[data-test=\"complete-header\"]").has(text("Thank you for your order!"));
+        $("button[data-test=\"back-to-products\"]").is(exist);
     }
 }
